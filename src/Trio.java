867ebbc7ab767e37bcd3fc5e6b1c73a789792735
BT_1N3_03 <ljaoutakas8934@bths.edu>
2018-11-21 14:19:48 -0500

    bxcvbxcvxcvcv

--- src/Trio.java
+++ src/Trio.java
@@ -8,5 +8,19 @@ public class Trio
         this.name = sandwich.getName() + "/" + salad.getName() + "/" + drink.getName() + " Trio";
         double h1 = sandwich.getPrice();
         double h2 = salad.getPrice();
+        double h3 = drink.getPrice();
+        if (h1 > h3 && h2 > h3) this.price = h1 + h2;
+        if (h1 > h2 && h3 > h2) this.price = h3 + h1;
+        if (h2 > h1 && h3 > h1) this.price = h2 + h3;
+    }
+
+    public double getPrice()
+    {
+        return this.price;
+    }
+
+    public String getName()
+    {
+        return this.name;
     }
 }
